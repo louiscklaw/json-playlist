50c7e88165667fb67371ca696280e91599ffd313
louiscklaw <louiscklaw@gmail.com>
2020-05-23 18:24:28 +0800

    tidy up script into queries file,

--- react-graphql-apollo-tryout/src/components/BookList.js
+++ react-graphql-apollo-tryout/src/components/BookList.js
@@ -1,21 +1,6 @@
-import React, {
-  Component
-} from 'react';
-import {
-  gql
-} from 'apollo-boost';
-import {
-  graphql
-} from 'react-apollo';
-
-const getBooksQuery = gql `
-    {
-        books {
-            name
-            id
-        }
-    }
-`;
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import { getBooksQuery } from '../queries/queries'
 
 class BookList extends Component {
   displayBooks() {
